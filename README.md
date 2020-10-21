c7df9edd5c0780f958edada093ca58bb2874e956
Clarissa Miranda <A01400214@itesm.mx>
2020-10-21 12:08:42 -0500

    separate evaluation predicition

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-Predicting football results in Spain's league through statistics.
+# FootballMLP
+
+FootballMLP is a framework which stands for Football Machine Learning Pipeline. By testing different Machine Learning Algorithms as well careffully studying football statistics, FootballMLP is able to predict football games and perform some extra tasks as described above.
+
+## What I need to have?
+Before using FootballMLP it is important to have the fixtures needed for the discriminating football results. Here is everything you need to install or perform previous executing the predictions.
+
+### Python packages
+FootballMLP needs the next libraries to work:
+- [Matplotlib] - 2D graphics library
+- [Numpy] - Scientific computation package
+- [Scipy] - Mathematics and science environment
+- [Sklearn] - Machine learning library
+- [Seaborn] - Data visualization library
+
+[Matplotlib]: <https://matplotlib.org/>
+[Numpy]: <https://numpy.org/>
+[Scipy]: <https://www.scipy.org/>
+[Sklearn]: <https://scikit-learn.org/stable/>
+[Seaborn]: <https://seaborn.pydata.org/>
+
+### Where is the data?
+Everything you want to retrieve can be find by using [API-Football](https://www.api-football.com/). But FootballMLP has released a library for it [apifootballpy](https://github.com/clarismiranda/apifootball/tree/lastFixtures)
+
+### Dataset for Standings
+This creates a whole dataset of teams' standings up to a season and a week (the last week played), the window parameter controls how many season before the current season wants to be save.\
+python maps_teams_stats.py --country --league --season --week --window
+```bash
+python maps_teams_stats.py DE 78 2020 4 5
+```
+> Note: by using a window of 5, it is expected to save the standings from 2016 to 2020.
+
+### Dataset for Fixtures
+This creates a whole dataset of league's fixtures up to a season and a week (the last week played) mapped with the team's last standings, the window parameter controls how many season before the current season wants to be save.\
+python maps_fixtures_stats.py --country --league --season --week --window
+```bash
+python maps_fixtures_stats.py DE 78 2020 4 5
+```
+> Note: by using a window of 5, it is expected to save the standings from 2016 to 2020.
+
+## Testing FootballMLP
+
+## Predicting with FootballMLP
--- ranking.py
+++ ranking.py
@@ -7,9 +7,13 @@ import pandas as pd
 
 class Attr:
     def __init__(self, name, value, value2=None, value3=None):
+        #This is the name of the team
         self.name = self.new_name(name)
+        # This is the cluster value
         self.value = value
+        # This is the id of the team
         self.value2 = self.val2(name)
+        # This is the season
         self.value3 = self.val3(name)
     def __str__(self):
         st = "name: " + self.name + ", value: " + str(self.value)
@@ -26,21 +30,34 @@ class Attr:
         return name[-4:]
 
 def get_value(attr):
+    """
+        Returns: the attribute value of the class Attr
+        which is the cluster
+    """
     return attr.value
 
 def sorted_class(names, values, color=None):
+    """
+        Returns: a list of names and values sorted (the cluster)
+    """
     attrs = []
     for i in range(0, len(names)):
         attrs.append(Attr(names[i], values[i]))
+    # Sorts by attribute value
     attrs.sort(key=get_value)
     new_names = []
     new_values = []
+    # Divides the names from the values
     for i in range(0, len(names)):
         new_names.append(attrs[i].name)
         new_values.append(attrs[i].value)
     return new_names, new_values
 
 def encode_data(df, df_column):
+    """
+        Returns: a numerical encoded column from its categorical value
+                df_column is the column to encode
+    """
     elements = np.unique(df[df_column].astype(str))
     print(len(elements))
     print(elements)
@@ -49,48 +66,65 @@ def encode_data(df, df_column):
     return enc.transform(df[df_column].astype(str))
 
 def encode_description(row):
+    """
+        Returns: the numerical value of a description
+    """
     label = {'Promotion - Champions League (Group Stage)': 1, 
              'UEFA Champions League': 1,
              'UEFA Champions League Qualifiers': 2,
-            'Promotion - Europa League (Group Stage)': 3,
+             'Promotion - Europa League (Group Stage)': 3,
              'UEFA Europa League': 3,
              'Promotion - Europa League (Qualification)': 4,
              'nan': 5,
              'Relegation - LaLiga2': 6,
              'Relegation': 6
             }
+    # If it doesn't have a promotion return regular team 5
     if row in label:
         return label[row]
     else:
         return 5
 
 def decode_description(row):
+    """
+        Returns: the categorical value of a description
+    """
     label = {1: 'Promotion - Champions League (Group Stage)', 
              1: 'UEFA Champions League',
              2: 'UEFA Champions League Qualifiers',
-            3: 'Promotion - Europa League (Group Stage)',
+             3: 'Promotion - Europa League (Group Stage)',
              3: 'UEFA Europa League',
              4: 'Promotion - Europa League (Qualification)',
              5: 'None',
              6: 'Relegation - LaLiga2',
              6: 'Relegation',
             }
+    # If it is a regular team returns 5
     if row in label:
         return label[row]
     else:
         return 'None'
 
 def score_home(best):
+    """
+        Returns: the value of the best streaks for home team on scores
+    """
     if best != 0:
         return int(best[0]) - int(best[2])
     return best
 
 def score_away(best):
+    """
+        Returns: the value of the best streaks for away team on scores
+    """
     if best != 0:
         return int(best[2]) - int(best[0])
     return best
 
 def clean_data(df, clean_type=None):
+    """
+        Returns: a clean dataframe with numerical values
+    """
     df = df.drop(df.columns[0], axis=1)
     # Ratio of games lose/played, draws/played
     df["stats_away.lose"] = df["stats_away.lose"]/df["stats_away.played"]
@@ -128,13 +162,21 @@ def clean_data(df, clean_type=None):
     return df
 
 def get_target(df, label):
+    """
+        Returns: a column from the specific label of the dataframe
+    """
     target = []
     for index, row in df.iterrows():
         target.append(row[label])
     return target
 
-# Label is the set of columns to keep as target
 def df_season(country, league, season, label, drop_others=None, clean_type=None):
+    """
+        Returns: the data of a given season and the names of the teams
+                label is the list columns to keep as target
+                drop_others a list of columns if neccesary to be removed
+                clean_type is either Home/Away/None
+    """
     dirCountry = '../' + country + '/'
     dirName = dirCountry + league + '/' + str(season) + '/'
     file_title = str(season) + '.csv'
@@ -142,17 +184,25 @@ def df_season(country, league, season, label, drop_others=None, clean_type=None)
     df = pd.read_csv(csv_file)
     df_names = df['team.id'].astype(str) + df['team.name']
     df_names = df_names.tolist()
+    # Retrieve only the standings of the data of study (Home/Away)
     df = clean_data(df, clean_type)
     target = []
+    # Saves the target
     for l in label:
         target = target + [get_target(df, l)]
+    # Removes the target
     df = df.drop(columns=label, axis=1)
+    # Extra columns to be remove
     if drop_others != None:
         df = df.drop(columns=drop_others, axis=1)
     return df, target, df_names
 
 def concat_data(country, league, seasons, target_col, clean_type):
+    """
+        Returns: a list of data tuples in a year window
+    """
     data = []
+    # Creates a row for each season with the form of tuple (season, data, target, name)
     for season in seasons:
         df, target, name = df_season(country, league, season, target_col, clean_type=clean_type)
         tup = (season, df, target, name)
@@ -160,22 +210,31 @@ def concat_data(country, league, seasons, target_col, clean_type):
     return data, df.columns
 
 def get_all_data(data, target_n):
+    """
+    Returns: the list of data, seasons, team's name and the target in a year window
+    """
     all_data = pd.DataFrame()
     all_season = []
     all_names = []
     all_target = [ [] for _ in range(target_n) ]
     for d in data:
+        # The data frame of the season
         all_data = all_data.append(d[1])
+        # Populates the season array with the actual season d[0]
         all_season = all_season + [d[0] for _ in range(len(d[1]))]
+        # The names of the teams in d[3]
         all_names = all_names + d[3]
         i = 0
+        # This is a 2D array of the given targets: ranking, points and promotion
         for target in d[2]:
             all_target[i] = all_target[i] + target
             i = i + 1
     return all_data, all_season, all_names, all_target
 
-# Returns a list of teams names with its season
 def label_team_season(all_names, all_season):
+    """
+    Returns: list of teams names with its season
+    """
     dendo_label = []
     for i in range(len(all_season)):
         d_l = all_names[i] + " - " + str(all_season[i])
@@ -183,27 +242,35 @@ def label_team_season(all_names, all_season):
     return dendo_label
 
 def dendogram_graph(data, best_method, label):
+    """
+    Plots the dendogram from Z linkage matrix
+    """
     label = [l[3:] for l in label]
+    # The resulting linkage of the best cophenetic distance
     Z = linkage(data, best_method)
     coph_matrix = cophenet(Z)
     fig = plt.figure(figsize=(25, 10))
+    # Creates dendrogram
     dendo = dendrogram(Z, leaf_rotation=90, labels=label)
     plt.title(best_method)
     plt.show()
     return (Z, coph_matrix)
 
 def HierarchicalClustering(data, label):
+    """
+    Finds the best linkage method and plots its dendogram
+    Returns: Z linkage matrix, cophenet matrix and best cophenetic distance
+    """
     methods = ["single","complete","average","centroid","ward"]
-
     # Pass the dataset into pdist to get your proximity matrix for calculating CPCC
     proximity_matrix = pdist(data)
-
     best_coph = -1
     best_method = None
-
+    # For each linkage method finds its cophenetic distance
     for method in methods:
         Z = linkage(data, method)
         coph, coph_matrix = cophenet(Z, proximity_matrix)
+        # Finds the minimum distance between clusters
         if coph > best_coph:
             best_coph = coph
             best_method = method
